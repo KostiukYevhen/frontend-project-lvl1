ac973b39e5d8d84dda09fb4c23323d4473277022
Yevhen Kostiuk <yevhenkostiuk@Yevhens-MacBook-Pro.local>
2019-09-06 13:11:12 +0300

    fixed isPrime algorithm in prime.js module

--- src/games/prime.js
+++ src/games/prime.js
@@ -5,12 +5,15 @@ import getRandomInteger from '../utils';
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (num) => {
-  for (let i = 2; i <= num; i += 1) {
+  if (num <= 1) {
+    return false;
+  }
+  for (let i = 2; i <= num / 2; i += 1) {
     if (num % i === 0) {
       return num === i;
     }
   }
-  return false;
+  return true;
 };
 
 const getCorrectAnswer = () => {
